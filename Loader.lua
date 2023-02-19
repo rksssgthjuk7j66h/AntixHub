149bc898e0b1a3098f582863fcbc1947b669ecf7
rksssgthjuk7j66h <95147076+rksssgthjuk7j66h@users.noreply.github.com>
2023-02-19 22:20:11 +0300

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,12 @@
+if game.PlaceId == 189707 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/rksssgthjuk7j66h/AntixHub/main/NaturalDisasterSurvival.lua"))()
+end
+if game.PlaceId == 1345139196 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/rksssgthjuk7j66h/AntixHub/main/TreasureHuntSimulator.lua"))()
+end
+if game.PlaceId == 286090429 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/rksssgthjuk7j66h/AntixHub/main/Arsenal.lua"))()
+end
+if game.PlaceId == 6516141723 or game.PlaceId == 6839171747 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/rksssgthjuk7j66h/AntixHub/main/Doors.lua"))()
+end
